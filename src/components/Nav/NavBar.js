0ea7086aa7392f79ab5dc604f599b761b6bfca00
rbgreenway3rd <3rdgreenway@gmail.com>
2024-10-03 15:14:56 -0500

    added assets folder to include WaveFront logos and other images.

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../../styles/NavBar.css";
-// import { Link, useHistory } from "react-router-dom";
+// import WaveGuideLogo from "../../assets/brand/WaveGuideLogo.gif"; // Import the gif file
+import WaveGuideLogo from "../../../src/assets/brand/WaveGuideLogo.gif"; // Absolute path from src
+import WaveGuideIcon from "../../../src/assets/brand/WaveGuideIcon.ico";
 
 export const NavBar = () => {
-  //   const history = useHistory();
-
   return (
     <header className="navbar-container">
       <h1 className="logo">Wave Explorer</h1>
+      <img src={WaveGuideIcon} alt="WaveGuide Icon" />{" "}
+      {/* Use the imported gif */}
     </header>
   );
 };
