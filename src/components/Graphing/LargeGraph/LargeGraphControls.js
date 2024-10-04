9936d62e78666ac578e35a20c69251f4000fbbd3
rbgreenway3rd <3rdgreenway@gmail.com>
2024-10-03 21:48:26 -0500

    updated large graph controls css to more closely match minigraph controls.

--- src/components/Graphing/LargeGraph/LargeGraphControls.js
+++ src/components/Graphing/LargeGraph/LargeGraphControls.js
@@ -15,101 +15,103 @@ export const LargeGraphControls = ({
 }) => {
   return (
     <div className="large-graph-controls">
-      <section className="zoom-controls">
-        <header className="zoom-controls-header">
-          Zoom
-          <input
-            type="checkbox"
-            id="zoom-state"
-            className="zoom-state-checkbox"
-            defaultChecked={true}
-            onChange={() => toggleZoomState(zoomState)}
-          />
-        </header>
-        <section className="zoom-controls-radios">
-          <label>
+      <section className="zoom-and-pan-controls">
+        <div className="zoom-controls">
+          <header className="zoom-controls-header">
+            Zoom
             <input
-              type="radio"
-              id="zoom-x"
-              className="zoom-controls__radio"
-              value="zoomX"
-              name="radio-group-zoom"
-              onChange={() => changeZoomMode("x")}
-            />
-            X
-          </label>
-          <label>
-            <input
-              type="radio"
-              id="zoom-y"
-              className="zoom-controls__radio"
-              value="zoomY"
-              name="radio-group-zoom"
-              onChange={() => changeZoomMode("y")}
-            />
-            Y
-          </label>
-          <label>
-            <input
-              type="radio"
-              id="zoom-xy"
-              className="zoom-controls__radio"
-              value="zoomXY"
-              name="radio-group-zoom"
+              type="checkbox"
+              id="zoom-state"
+              className="zoom-state-checkbox"
               defaultChecked={true}
-              onChange={() => changeZoomMode("xy")}
+              onChange={() => toggleZoomState(zoomState)}
             />
-            X-Y
-          </label>
-        </section>
-      </section>
-      <section className="pan-controls">
-        <header className="pan-controls-header">
-          Pan
-          <input
-            type="checkbox"
-            id="pan-state"
-            className="pan-state-checkbox"
-            defaultChecked={true}
-            onChange={() => togglePanState(panState)}
-          />
-        </header>
-        <section className="pan-controls-radios">
-          <label>
+          </header>
+          <section className="zoom-controls-radios">
+            <label>
+              <input
+                type="radio"
+                id="zoom-x"
+                className="zoom-controls__radio"
+                value="zoomX"
+                name="radio-group-zoom"
+                onChange={() => changeZoomMode("x")}
+              />
+              X
+            </label>
+            <label>
+              <input
+                type="radio"
+                id="zoom-y"
+                className="zoom-controls__radio"
+                value="zoomY"
+                name="radio-group-zoom"
+                onChange={() => changeZoomMode("y")}
+              />
+              Y
+            </label>
+            <label>
+              <input
+                type="radio"
+                id="zoom-xy"
+                className="zoom-controls__radio"
+                value="zoomXY"
+                name="radio-group-zoom"
+                defaultChecked={true}
+                onChange={() => changeZoomMode("xy")}
+              />
+              X-Y
+            </label>
+          </section>
+        </div>
+        <div className="pan-controls">
+          <header className="pan-controls-header">
+            Pan
             <input
-              type="radio"
-              id="pan-x"
-              className="pan-controls__radio"
-              value="panX"
-              name="radio-group-pan"
-              onChange={() => changePanMode("x")}
-            />
-            X
-          </label>
-          <label>
-            <input
-              type="radio"
-              id="pan-y"
-              className="pan-controls__radio"
-              value="panY"
-              name="radio-group-pan"
-              onChange={() => changePanMode("y")}
-            />
-            Y
-          </label>
-          <label>
-            <input
-              type="radio"
-              id="pan-xy"
-              className="pan-controls__radio"
-              value="panXY"
-              name="radio-group-pan"
+              type="checkbox"
+              id="pan-state"
+              className="pan-state-checkbox"
               defaultChecked={true}
-              onChange={() => changePanMode("xy")}
+              onChange={() => togglePanState(panState)}
             />
-            X-Y
-          </label>
-        </section>
+          </header>
+          <section className="pan-controls-radios">
+            <label>
+              <input
+                type="radio"
+                id="pan-x"
+                className="pan-controls__radio"
+                value="panX"
+                name="radio-group-pan"
+                onChange={() => changePanMode("x")}
+              />
+              X
+            </label>
+            <label>
+              <input
+                type="radio"
+                id="pan-y"
+                className="pan-controls__radio"
+                value="panY"
+                name="radio-group-pan"
+                onChange={() => changePanMode("y")}
+              />
+              Y
+            </label>
+            <label>
+              <input
+                type="radio"
+                id="pan-xy"
+                className="pan-controls__radio"
+                value="panXY"
+                name="radio-group-pan"
+                defaultChecked={true}
+                onChange={() => changePanMode("xy")}
+              />
+              X-Y
+            </label>
+          </section>
+        </div>
       </section>
       <button className="reset-zoom-button" onClick={resetZoom}>
         Reset Zoom
