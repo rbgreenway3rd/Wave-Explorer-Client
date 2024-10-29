b59aca30ef091b2e8b0cbbcc273fc1a021c6126e
rbgreenway3rd <3rdgreenway@gmail.com>
2024-10-29 15:55:51 -0500

    moved utility functions from heatmap into their own module.

---
+++ src/components/Graphing/Metrics/MetricsUtilities.js
@@ -0,0 +1,69 @@
+// MetricsUtilities.js
+import * as d3 from "d3";
+
+export const linearRegression = (data) => {
+  let xsum = 0,
+    ysum = 0;
+  for (let i = 0; i < data.length; i++) {
+    xsum += data[i].x;
+    ysum += data[i].y;
+  }
+  const xmean = xsum / data.length;
+  const ymean = ysum / data.length;
+
+  let num = 0,
+    denom = 0;
+  for (let i = 0; i < data.length; i++) {
+    const x = data[i].x;
+    const y = data[i].y;
+    num += (x - xmean) * (y - ymean);
+    denom += (x - xmean) * (x - xmean);
+  }
+  return num / denom;
+};
+
+export const calculateSlope = (heatmapData) => {
+  return heatmapData.length > 0 ? linearRegression(heatmapData) : 0;
+};
+
+export const calculateRange = (heatmapData) => {
+  if (heatmapData.length === 0) return 0; // If no data, return 0
+
+  const maxY = d3.max(heatmapData, (d) => d.y);
+  const minY = d3.min(heatmapData, (d) => d.y);
+
+  return maxY - minY; // Return the range (difference between max and min)
+};
+
+export const getAllValues = (wellArrays, annotationRange) => {
+  return wellArrays.flatMap((well) => {
+    const filteredData = well.indicators[0]?.filteredData || [];
+    return filteredData
+      .filter((_, i) => i >= annotationRange.start && i <= annotationRange.end)
+      .map((d) => d.y);
+  });
+};
+
+export const getAllSlopes = (wellArrays, annotationRange) => {
+  return wellArrays.map((well) => {
+    let heatmapData = well.indicators[0]?.filteredData || [];
+    if (annotationRange.start !== null && annotationRange.end !== null) {
+      heatmapData = heatmapData.filter(
+        (_, i) => i >= annotationRange.start && i <= annotationRange.end
+      );
+    }
+    return calculateSlope(heatmapData);
+  });
+};
+
+export const getAllRanges = (wellArrays, annotationRange) => {
+  return wellArrays.map((well) => {
+    let heatmapData = well.indicators[0]?.filteredData || [];
+    if (annotationRange.start !== null && annotationRange.end !== null) {
+      heatmapData = heatmapData.filter(
+        (_, i) => i >= annotationRange.start && i <= annotationRange.end
+      );
+    }
+    return calculateRange(heatmapData);
+  });
+};
