a417c5479f0685c4870fe6f17bf112bc18ce4f93
rbgreenway3rd <3rdgreenway@gmail.com>
2024-10-28 23:22:10 -0500

    small css adjustments

--- src/components/Graphing/Metrics/MetricsControls.js
+++ src/components/Graphing/Metrics/MetricsControls.js
@@ -1,25 +1,3 @@
-// import React, { useContext, useEffect, useState, useRef } from "react";
-// import "./MetricsControls.css";
-// export const MetricsControls = () => {
-//   return (
-//     <div className="metrics__controls-container">
-//       <div className="metrics__radio-container">
-//         Show
-//         <div className="radio__show-max">
-//           <input
-//             type="radio"
-//             id="show-max"
-//             className="radio__show-max-radio"
-//             value="showMax"
-//             name="radio-group-metrics"
-//             // defaultChecked={true}
-//           />
-//           <label htmlFor="show-raw">Max</label>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 import React, { useState } from "react";
 import "./MetricsControls.css";
 
